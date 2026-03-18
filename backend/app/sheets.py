b641e40c005e9332889060c45f58f23079503184
harshcodeoftenly <harshvardhan1483@gmail.com>
2026-03-18 13:43:10 +0530

    Changed in Sheets file

--- backend/app/sheets.py
+++ backend/app/sheets.py
@@ -1,21 +1,30 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import os
+import json
+from oauth2client.service_account import ServiceAccountCredentials
 
 def save_to_sheet(data):
-    scope = ["https://spreadsheets.google.com/feeds", 
-             "https://www.googleapis.com/auth/drive"]
+    scope = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/drive"
+    ]
 
-    creds = ServiceAccountCredentials.from_json_keyfile_name(
-        "credentials.json", scope
-    )
+    if os.getenv("GOOGLE_CREDS"):
+        # 🌍 Production (Render)
+        creds_dict = json.loads(os.getenv("GOOGLE_CREDS"))
+        creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+    else:
+        # 💻 Local
+        creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
 
     client = gspread.authorize(creds)
 
     sheet = client.open("Appointments").get_worksheet(0)
 
     sheet.append_row([
-        data["name"],
-        data["date"],
-        data["time"],
-        data["purpose"]
+        data.get("name"),
+        data.get("date"),
+        data.get("time"),
+        data.get("purpose")
     ])
